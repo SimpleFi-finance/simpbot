4ae0b238fb38377bce29626ab760bdd6950deee1
filvecchiato <fil.vecchiato@gmail.com>
2021-10-14 18:20:52 +0100

    update db connection

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const {Sequelize, Op} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.PASSWORD, {
 	host: process.env.DB_HOST,
-	dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 const Users = sequelize.define('users', {

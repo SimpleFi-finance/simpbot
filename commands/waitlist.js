13498b3b9b2922c91d322f2ef21914055bd00240
Raphael <raphael.mazet@gmail.com>
2021-11-09 14:46:41 +0000

    Start fixing DM issue on waitlist command

--- commands/waitlist.js
+++ commands/waitlist.js
@@ -10,7 +10,6 @@ module.exports = {
 	async execute(interaction) {
 
     const waitlist = await Lists.findOne({ where: { name: process.env.WAITLIST_NAME } });
-    console.log(waitlist);
     const accessSize = waitlist.dataValues.size;
 
     //Set correct channel id for users to create interaction
@@ -20,10 +19,15 @@ module.exports = {
     }
 
     const {username, discriminator, id} = interaction.user;
-    // equivalent to: SELECT * FROM users WHERE name = 'handle' LIMIT 1;
+    //Set up something
+    let user, newUser;
+    // Create flag for potential error message
+    let userHasAccess = false;
 
     try {
-      const user = await Users.findOne({ where: { handle: username + discriminator } });
+      // equivalent to: SELECT * FROM users WHERE name = 'handle' LIMIT 1;
+      user = await Users.findOne({ where: { handle: username + discriminator } });
+      console.log(' ---> user', user);
 
       // Deal with user who already registered
       if (user) {
@@ -36,6 +40,7 @@ module.exports = {
         const userPosition = users.findIndex(el => el.passCode === user.passCode);
 
         if (userPosition + 1 <= accessSize) {
+          userHasAccess = true;
           await interaction.user.send(`You already have access and your code is ${user.dataValues.passCode}`)
           await interaction.reply(`${interaction.user} you already have access silly! Check your DMs.`);
         } else {
@@ -49,24 +54,29 @@ module.exports = {
           numbers: true
         });
         // register user on DB - equivalent to: INSERT INTO users (handle, userId, passcode) values (?, ?, ?);
-        const newUser = await Users.create({
+        newUser = await Users.create({
           handle: `${username}${discriminator}`,
           userId: id,
           passCode: passCode
         });
+        console.log(' ---> newUser', newUser);
 
         const users = await Users.findAll({
           order: [
             ['id', 'ASC']
           ]
         });
+        console.log(' ---> users', users);
 
+        //works on db index not userID - check with increase-beta
         const userPosition = users.findIndex(el => el.passCode === passCode);
+        console.log(' ---> userPosition', userPosition);
 
         if (userPosition + 1 <= accessSize) {
+          userHasAccess = true;
           const betaRole = interaction.guild.roles.cache.find(r => r.name === 'beta tester');
           //give the new user access to beta-testers channel
-          await interaction.member.roles.add(betaRole.id);
+          await interaction.member.roles.add(betaRole);
           await interaction.user.send(
             "You've been authorised. Your access code is " + passCode +
             "\nYou now have access to the beta-testers channel. Please give us your feedback!");
@@ -78,8 +88,18 @@ module.exports = {
       }
 
     } catch (error) {
-      console.error('Waitlist error -->', error);
-      await interaction.reply('Oops something went wrong. Please DM a team member.');
+
+      console.error('Waitlist error --->', error);
+
+      if (error.message === 'Cannot send messages to this user') {
+        const privacyReply = `Hi ${interaction.user} I can't DM you with your access code 😢 You may need to allow DMs from this server's members.`
+        const dbConfirmReply = "I checked and you're on the waitlist though! Contact a member of the team to get your position."
+
+          await interaction.reply(`${privacyReply} ${userHasAccess && dbConfirmReply}`);
+
+      } else {
+        await interaction.reply('Oops something went wrong. Please DM a team member.');
+      }
     }
   }
 }
